f4ae2240113d46ef2b11f54d81077ec36222d38f
Carlos Flynn <18237370+Sotilrac1@users.noreply.github.com>
2023-08-10 13:36:28 +0000

    Update Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
-import { useState } from 'react/cjs/react.production.min';
+
 
 const Expenses = (props) => {
-  const [filteredYear,setFilteredYear] = useState("2020")
+
+  const [filteredYear,setFilteredYear] = useState('2020');
+
   function filterChangeHandler(selectedYear){
       setFilteredYear(selectedYear);
   };
 
   return (
-     
+     <div>
+        
        <Card className="expenses">
-
-          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
           <ExpenseItem
             title={props.items[0].title}
             amount={props.items[0].amount}
@@ -38,7 +40,7 @@ const Expenses = (props) => {
             date={props.items[3].date}
           />
           </Card>
-    
+    </div>
   );
 }
 
